bf8dbf0aee32932e40e27ef4f3c8f67e2e44bc89
Dhruva Sambrani <44899822+DhruvaSambrani@users.noreply.github.com>
2020-04-08 01:11:20 +0530

    Move to photon

--- src/main/app.js
+++ src/main/app.js
@@ -1,8 +1,6 @@
 const{app, BrowserWindow, ipcMain, BrowserView, Menu, MenuItem} = require('electron')
 const path = require('path')
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 var mainWindow = null;
 var view = null;
 
@@ -57,18 +55,13 @@ const menu = Menu.buildFromTemplate([
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform != 'darwin') {
         app.quit();
     }
 });
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
 app.on('ready', function() {
-    // Create the browser window.
-    Menu.setApplicationMenu(menu)
+    Menu.setApplicationMenu(null);
     mainWindow = new BrowserWindow({
         width: 100,
         height: 300,
@@ -84,19 +77,12 @@ app.on('ready', function() {
     });
     mainWindow.once('ready-to-show', () => {
         mainWindow.maximize();
-        mainWindow.show()
+        mainWindow.show();
     })
 
-    // and load the index.html of the app.
     mainWindow.loadURL('file://' + __dirname + '/index.html');
 
-    // Open the DevTools.
-    //mainWindow.openDevTools();
-    // Emitted when the window is closed.
     mainWindow.on('closed', function() {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
         mainWindow = null;
     });
 });
--- src/main/js/main.js
+++ src/main/js/main.js
@@ -1,7 +1,8 @@
 const ipc = require('electron').ipcRenderer;
-const webview = document.getElementById('iframe');
+const webview = document.getElementById('main');
 const maindiv = document.getElementById('maindiv');
 const settdiv = document.getElementById('settdiv');
+const photon = require('electron-photon');
 function openPage(URL){
     webview.src = URL;
     maindiv.style.display="inherit";
@@ -12,12 +13,18 @@ function openSettings() {
     settdiv.style.display="inherit";
     maindiv.style.display="none";
 }
+function back() {
+    webview.canGoBack()?webview.goBack():console.log('bottom of stack');
+}
+function forward() {
+    webview.canGoForward()?webview.goForward():console.log('top of stack');
+}
 ipc.on('nav', (event, message) => {
     if(message=="back"){
-        webview.canGoBack()?webview.goBack():console.log('bottom of stack');
+        back();
     }
     if(message=="forward"){
-        webview.canGoForward()?webview.goForward():console.log('top of stack');
+        forward();
     }
     if(message=="reload"){
         webview.reload();
